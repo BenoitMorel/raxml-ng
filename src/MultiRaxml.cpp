fc6c223b5ccf50d03b1bec867dbe7d780c37a483
BenoitMorel <benomorel@gmail.com>
2018-01-15 18:40:51 +0100

    fix deadlock with Irecv, fix the communcation tags, use local communicators when required

--- src/MultiRaxml.cpp
+++ src/MultiRaxml.cpp
@@ -34,13 +34,13 @@ public:
   {
     istringstream is(command_str);
     is >> _threadsNumber;
-    LOG_INFO << "Threads : " << _threadsNumber << endl;
+    //LOG_INFO << "Threads : " << _threadsNumber << endl;
     _args.push_back("raxml");
     while (is) {
       string str;
       is >> str;
       _args.push_back(str);
-      LOG_INFO << str << endl;
+   //   LOG_INFO << str << endl;
     }
   }
 
@@ -80,55 +80,72 @@ unsigned int currentCommand = 0;
 void *master_thread(void *) {
   while(true) {
     MPI_Status stat;
+    MPI_Request request;
     int tmp;
-    MPI_Recv(&tmp, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+    int flag = false;
+    MPI_Irecv(&tmp, 1, MPI_INT, MPI_ANY_SOURCE, 42, MPI_COMM_WORLD, &request);
+    MPI_Test(&request, &flag, &stat); 
+    while(!flag) {
+      std::this_thread::sleep_for (std::chrono::milliseconds(20));
+      MPI_Test(&request, &flag, &stat); 
+    }
     if (tmp == -1) {
       return 0;
     }
-    MPI_Send(&currentCommand, 1, MPI_INT, stat.MPI_SOURCE, 0, MPI_COMM_WORLD);
+    std::cerr << "m send " << currentCommand << std::endl; 
+    MPI_Send(&currentCommand, 1, MPI_INT, stat.MPI_SOURCE, 42, MPI_COMM_WORLD);
+    std::cerr << "m sent " << currentCommand << std::endl; 
     currentCommand++;
     std::this_thread::sleep_for (std::chrono::milliseconds(20));
   }
   return 0;
 }
 
+int getRank(MPI_Comm comm)
+{
+  int res;
+  MPI_Comm_rank(comm, &res);
+  return res;
+}
 
 int requestCurrentCommand() {
-  if (ParallelContext::proc_id()) {
+  if (getRank(MPI_COMM_WORLD)) {
     int res = -1;
     int tmp = 0;
     MPI_Status stat;
-    MPI_Sendrecv(&tmp, 1, MPI_INT, 0, 0, &res, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+    std::cerr << "s ask " << std::endl; 
+    MPI_Sendrecv(&tmp, 1, MPI_INT, 0, 42, &res, 1, MPI_INT, 0, 42, MPI_COMM_WORLD, &stat);
+    std::cerr << "s got " << res << std::endl; 
     return res;
   } else {
     return currentCommand++;
   }
 }
 
-void *slaves_thread(void *) {
-  for (unsigned int i = 0; i < 5; ++i) {
-    std::this_thread::sleep_for (std::chrono::seconds(1));
-    std::cout << "p" << ParallelContext::proc_id() << " c" << requestCurrentCommand() << std::endl;
+int getCurrentCommand(MPI_Comm comm) {
+  unsigned int currentCommand = 0;
+  if (!getRank(comm)) {
+    currentCommand = requestCurrentCommand();
+  }
+  std::cerr << "bef bcast" << std::endl;
+  MPI_Bcast(&currentCommand, 1, MPI_INT, 0, comm);
+  std::cerr << "aft bcast" << std::endl;
+  return currentCommand;
+}
+
+void slaves_thread(const vector<RaxmlCommand *> &commands, MPI_Comm comm) {
+  int command = 0;
+  while ((command = getCurrentCommand(comm)) < commands.size()) {
+    std::cerr << "Proc " << getRank(MPI_COMM_WORLD) << " " << " command " << command << std::endl;
+    std::this_thread::sleep_for (std::chrono::milliseconds(1000));
+    MPI_Barrier(comm);
+    //std::cerr << "after barrier" << std::endl;
   }
   /*
-  string input_file = argv[1]; 
-  vector<string> commands_str;
-  read_commands_file(input_file, commands_str);
-  vector<RaxmlCommand *> commands;
-  for (const auto &str: commands_str) {
-    commands.push_back(new RaxmlCommand(str));
-  }
-  std::sort(commands.rbegin(), commands.rend(), RaxmlCommand::comparePtr);
-  
   for (auto command: commands) {
     command->run(MPI_COMM_WORLD);
   }
-  
-  for (auto command: commands) {
-    delete command;
-  }
   */
-  return 0;
 }
 
 int multi_raxml(int argc, char** argv)
@@ -145,14 +162,26 @@ int multi_raxml(int argc, char** argv)
     pthread_t master;
     pthread_create(&master, 0, master_thread, 0);
   }
-  slaves_thread(0);
+  string input_file = argv[1]; 
+  vector<string> commands_str;
+  read_commands_file(input_file, commands_str);
+  vector<RaxmlCommand *> commands;
+  for (const auto &str: commands_str) {
+    commands.push_back(new RaxmlCommand(str));
+  }
+  std::sort(commands.rbegin(), commands.rend(), RaxmlCommand::comparePtr); 
   
+  slaves_thread(commands, MPI_COMM_WORLD);
+  
+  for (auto command: commands) {
+    delete command;
+  }
   
   LOG_INFO << "todobenoit: remove this horrible termination hack" << std::endl;
   std::this_thread::sleep_for (std::chrono::milliseconds(1000));
   if (1 == ParallelContext::proc_id()) {
     int stop = -1;
-    MPI_Send(&stop, 1, MPI_INT, 0, 0, MPI_COMM_WORLD); 
+    MPI_Send(&stop, 1, MPI_INT, 0, 42, MPI_COMM_WORLD); 
   }
   std::this_thread::sleep_for (std::chrono::milliseconds(1000));
   return 0; 
