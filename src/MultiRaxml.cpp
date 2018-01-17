5ea9be28f3316abc6f329d1dd2104c373a763d22
BenoitMorel <benomorel@gmail.com>
2018-01-17 10:43:43 +0100

    Also print date/hour of start to d differenciate my experiments

--- src/MultiRaxml.cpp
+++ src/MultiRaxml.cpp
@@ -10,9 +10,10 @@
 #include <cmath>
 #include <sstream>
 #include <algorithm>
-#include <iostream>       // std::cout, std::endl
-#include <thread>         // std::this_thread::sleep_for
-#include <chrono>         // std::chrono::seconds
+#include <iostream>       
+#include <thread>         
+#include <chrono>     
+#include <ctime>     
 #include "log.hpp"
 #include "ParallelContext.hpp"
 
@@ -40,7 +41,7 @@ int getSize(MPI_Comm comm)
 
 int sitesToThreads(int sites)
 {
-  int threads = std::max(sites, 1000) / 1000;
+  int threads = max(sites, 1000) / 1000;
   // lowest power of 2
   return pow(2,floor(log(threads)/log(2)));
 }
@@ -54,7 +55,7 @@ public:
 
   }
 
-  RaxmlCommand(const std::string &command_str) :
+  RaxmlCommand(const string &command_str) :
     _threadsNumber(0),
     _args(0)
   {
@@ -65,7 +66,7 @@ public:
       is >> str;
       _args.push_back(str);
     }
-    _argv = std::vector<char *>(_args.size() + 1, 0);
+    _argv = vector<char *>(_args.size() + 1, 0);
     for (unsigned int i = 0; i < _args.size(); ++i) {
       _argv [i + 1] = (char *)_args[i].c_str(); //todobenoit const hack here
     }
@@ -74,7 +75,6 @@ public:
         get_dimensions(_argv.size() - 1, &_argv[0], _sites, _tips));
 
     _threadsNumber = sitesToThreads(_sites);
-    //std::cout << _sites << " sites -> " << _threadsNumber << " threads" << std::endl;
   }
 
   void run(MPI_Comm comm, MPI_Comm globalComm) const {
@@ -91,7 +91,7 @@ public:
     raxml(_argv.size() - 1, (char **)&_argv[0]); // todobenoit const hack here
   }
   
-  std::string getDebugStr(int globalRank, int threads) const {
+  string getDebugStr(int globalRank, int threads) const {
     ostringstream os;
     if (!_valid) {
       os << "Invalid command: ";
@@ -101,7 +101,7 @@ public:
     for (const auto &str: _args) {
       os << str << " ";
     }
-    os << std::endl;
+    os << endl;
     return os.str();
   }
 
@@ -117,7 +117,7 @@ public:
 private:
   int _threadsNumber;
   vector<string> _args;
-  std::vector<char *> _argv;
+  vector<char *> _argv;
   int _sites;
   int _tips;
   bool _valid;
@@ -146,15 +146,15 @@ void *master_thread(void *d) {
     int tmp = 0;
     MPI_Recv(&tmp, 1, MPI_INT, MPI_ANY_SOURCE, MPI_TAG_GET_CMD, globalComm, &stat);
     if (tmp == MPI_SIGNAL_KILL_MASTER) {
-      std::cout << "thread_master received kill signal" << std::endl;
+      cout << "thread_master received kill signal" << endl;
       return 0;
     } else if (tmp == MPI_SIGNAL_GET_CMD) {
-      std::cout << "thread master sends command " << currentCommand <<
-        " to rank " << stat.MPI_SOURCE << std::endl;
+      cout << "thread master sends command " << currentCommand <<
+        " to rank " << stat.MPI_SOURCE << endl;
       MPI_Send(&currentCommand, 1, MPI_INT, stat.MPI_SOURCE, MPI_TAG_GET_CMD, globalComm);
       currentCommand++;
     } else {
-      std::cerr << "Error in master_thread: unexpected signal " << tmp << std::endl; 
+      cerr << "Error in master_thread: unexpected signal " << tmp << endl; 
     }
   }
   return 0;
@@ -174,17 +174,17 @@ int getCurrentCommand(MPI_Comm globalComm, MPI_Comm localComm) {
   return currentCommand;
 }
 
-void readCommands(const std::string &input_file, RaxmlCommands &commands)
+void readCommands(const string &input_file, RaxmlCommands &commands)
 {
   vector<string> commands_str;
   read_commands_file(input_file, commands_str);
   for (const auto &str: commands_str) {
     commands.push_back(shared_ptr<RaxmlCommand>(new RaxmlCommand(str)));
   }
-  std::sort(commands.rbegin(), commands.rend(), RaxmlCommand::comparePtr); 
+  sort(commands.rbegin(), commands.rend(), RaxmlCommand::comparePtr); 
 }
 
-void slaves_thread(const std::string &input_file, MPI_Comm globalComm, MPI_Comm localComm) {
+void slaves_thread(const string &input_file, MPI_Comm globalComm, MPI_Comm localComm) {
   RaxmlCommands commands;
   int currCommand = 0;
   readCommands(input_file, commands);
@@ -210,10 +210,10 @@ void slaves_thread(const std::string &input_file, MPI_Comm globalComm, MPI_Comm 
 int multi_raxml(int argc, char** argv)
 {
   if (argc != 2) {
-    std::cout << "Invalid syntax: multi-raxml requires one argument" << endl;
+    cout << "Invalid syntax: multi-raxml requires one argument" << endl;
     return 0;
   }
-  Timer begin = std::chrono::system_clock::now();;
+  Timer begin = chrono::system_clock::now();;
   MPI_Comm globalWorld = MPI_COMM_WORLD;
   int globalRank = getRank(globalWorld);
   MASTER_RANK = getSize(globalWorld) - 1;
@@ -221,6 +221,8 @@ int multi_raxml(int argc, char** argv)
   MPI_Comm_split(MPI_COMM_WORLD, globalRank == MASTER_RANK, globalRank, &localWorld);
   
   if (MASTER_RANK == globalRank) {
+    time_t beginTime = chrono::system_clock::to_time_t(begin);
+    cout << "Starting time: " << ctime(&beginTime) << endl;
     master_thread(globalWorld);
   } else {
     string input_file = argv[1]; 
@@ -231,10 +233,10 @@ int multi_raxml(int argc, char** argv)
     MPI_Send(&MPI_SIGNAL_KILL_MASTER, 1, MPI_INT, MASTER_RANK, MPI_TAG_GET_CMD, globalWorld); 
   }
   if (MASTER_RANK == globalRank) {
-    Timer end = std::chrono::system_clock::now();
+    Timer end = chrono::system_clock::now();
     int elapsed_sec = chrono::duration_cast<chrono::seconds>
       (end-begin).count();
-    std::cout << "Elapsed time : " << elapsed_sec << "s" << std::endl;
+    cout << "Elapsed time : " << elapsed_sec << "s" << endl;
   }
   return 0; 
 }
